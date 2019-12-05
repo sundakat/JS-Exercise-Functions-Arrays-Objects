c81498ff7e565ab5920943732258e3dcbf10f162
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2019-12-04 20:44:44 -0600

    done getName

--- index.js
+++ index.js
@@ -61,9 +61,12 @@ makePersonObject(5, "Leia", "leia@leia.com");
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName({
+  name
+}) {
+  return `Hello, my name is ${name}`
 }
+
 
 /**
  * ### Challenge `makeSmartPerson`
